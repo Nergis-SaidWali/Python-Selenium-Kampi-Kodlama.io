b90b4334686c435f7d23e2b28674907256c795a1
Nergis-SaidWali <nergis.879018@gmail.com>
2023-03-09 17:52:00 +0300

    Odev01

---
+++ odev02.py
@@ -0,0 +1,61 @@
+# --Soru 1-- 
+# Pyhton veri tipleri
+# .............................................................................
+
+# String (str):
+	# Metinsel verileri saklama noktasında kolaylık olması için 'char' veri tiplerinden bir araya getirilerek oluşturulmuş bir veri tipidir. Aslında bir char 'array'idir.
+	# Python'un temelinde yer alan C dilinde char[] (veya char*) şeklinde ifade edilen String'ler aslında referans tutuculardır. Ancak bizim bilmemiz gereken kısım 
+	# bunların metinsel verileri tuttuğudur.
+
+# İnteger (int):
+	# Sayısal verileri tutmak için kullanılan bir veri tipidir. Sayıları tamsayı (Z) şeklinde tutar. Eğer integer bir değişkene ondalıklı bir sayı
+	# (1,3, -15,86 vb.) atanırsa sadece tam kısmını alır (1, -15 vb).
+
+# Float (float):
+	# Gerçel sayısal verileri tutmak için kullanılır. İnteger'ın aksine reel (R) olan tüm sayıları kapsar.
+
+# List (list):
+	# Birden fazla veriyi tek bir değişkende tutmamıza yarayan bir veri tipidir. İçinde tuttuğu verilerin Python'da aynı tür olmasına gerek yoktur.
+
+# Tuple (tuple):
+	# List veri tipi ile neredeyse aynı mantıkla çalışır ancak tuple 'immutable' yani değiştirilemez veri tipidir. Yani tuple'a ekleme, silme, güncelleme yapamazsınız.
+
+# Dictionary (dict):
+	# Türkçesi 'sözlük' anlamına gelen Dictionary, birbiriyle 'key, value' ilişkisi ile eşleşen verilerin bir arada tutulmasını sağlar.
+	# Örneğin bir sözlüğü simüle edersek: (<key>, <value>) ("araba", "car") şeklinde tutulur.
+
+# Boolean (bool):
+	# Bilgisayarın temel yapı taşı olan bit'lerden bir tanesinde saklanan veri tipidir. Bit'ler sadece 1 veya 0 değerlerini alabilirler.
+	# Dolayısıyla bool veri tipi de sadece 1 veya 0 değerini alabilir. Python'da 1, True; 0, False olarak ifade edilebilir. Sadece 1 bit yer kaplar.
+
+# .............................................................................
+
+# --Soru 2-- 
+# Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz?
+
+# .............................................................................
+
+yetistirmeKampi="Python ve Selenium"  #String veri tipi
+gun=1  #int veri tipi
+
+# .............................................................................
+
+# --Soru 3--
+# Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz?
+# .............................................................................
+
+user = "mrxa"
+password = "0000"
+ 
+print("Kullanici adinizi girin: ")
+x = str(input());
+
+print("Sifrenizi girin: ")
+y = str(input());
+ 
+if user == x and password == y:
+    print("--Giris yapilmistir--")
+elif user!= x or password !=y:
+    print("**Kullanici adi veya sifre hatali**")
+
+# .............................................................................
